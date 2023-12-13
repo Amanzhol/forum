efa147572a046eeb9a6fb97b4a0ca50b655842db
medetov.amanzhol <medetov.amanzhol@alser.kz>
2023-12-13 12:55:41 +0600

    Migrations & Models

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
